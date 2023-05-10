589a0e75a60bb97eb021e7858e0090ca78ab0d7f
shehjad-dev <satauskz@gmail.com>
2023-05-11 02:52:16 +0600

    fix: slider link issues and footer issues fixed

--- src/components/UseCaseSection/SliderNav.jsx
+++ src/components/UseCaseSection/SliderNav.jsx
@@ -1,20 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const SliderNav = ({ carouselData }) => {
     const [activeLinkIndex, setActiveLinkIndex] = useState(0);
-    const handleClick = (index) => {
+    const scrollContainerRef = useRef(null);
+
+    const handleClick = (index, event) => {
+        event.preventDefault();
         setActiveLinkIndex(index);
+
+        const targetId = `slide-${index + 1}`;
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            const containerElement = targetElement.parentNode;
+            const containerRect = containerElement.getBoundingClientRect();
+            const targetRect = targetElement.getBoundingClientRect();
+            const scrollTo =
+                targetRect.left -
+                containerRect.left +
+                containerElement.scrollLeft;
+
+            containerElement.scrollTo({
+                left: scrollTo,
+                behavior: "smooth",
+            });
+        }
     };
     return (
         <>
             {carouselData.map((item, index) => (
                 <a
-                    href={`#slide-${index + 1}`}
+                    /* href={`#slide-${index + 1}`} */
                     key={index}
                     className={`buttonLinks ${
                         index === activeLinkIndex ? "activeCarouselNavitem" : ""
                     }`}
-                    onClick={() => handleClick(index)}
+                    onClick={(event) => handleClick(index, event)}
                 >
                     {item.title}
                 </a>
